9b9189d0ee6fdc73bc36e5bff1bd6fa83e0feb15
Lars Kellogg-Stedman <lars@oddbit.com>
2020-03-03 10:20:44 -0500

    use volume.volumes() rather than calling the endpoint directly

--- cinder_exporter/collector.py
+++ cinder_exporter/collector.py
@@ -14,36 +14,34 @@ class CinderCollector(object):
     def __init__(self, cloud):
         self.cloud = cloud
 
-    def volumes(self):
-        endpoint = self.cloud.endpoint_for('volume')
-        url = '{}/volumes/detail'.format(endpoint)
-        res = self.cloud.session.get(url, params={'all_tenants': 'true'})
-        res.raise_for_status()
-        return res.json().get('volumes', [])
-
     def get_volume_metrics(self):
         vol_info = GaugeMetricFamily(
             'cinder_volume_info',
             'Info about a volume',
             labels=[
-                'cinder_vol_uuid', 'cinder_vol_name', 'cinder_vol_type',
-                'cinder_vol_bootable', 'cinder_vol_status',
-                'os_project_uuid', 'os_user_uuid',
+                'cinder_vol_uuid',
+                'cinder_vol_name',
+                'cinder_vol_type',
+                'cinder_vol_bootable',
+                'cinder_vol_status',
+                'os_project_uuid',
             ])
         vol_size = GaugeMetricFamily(
             'cinder_volume_size_gb',
             'Size of a volume',
             labels=['cinder_vol_uuid'])
 
-        for vol in self.volumes():
-            LOG.debug('getting information for volume %s', vol['id'])
+        for vol in self.cloud.volume.volumes(all_projects=True):
+            LOG.debug('getting information for volume %s', vol.id)
             vol_info.add_metric([
-                vol['id'], vol['name'], vol['volume_type'], vol['bootable'],
-                vol['status'],
-                vol['os-vol-tenant-attr:tenant_id'],
-                vol['user_id'],
+                vol.id,
+                vol.name,
+                vol.volume_type,
+                'true' if vol.is_bootable else 'false',
+                vol.status,
+                vol.project_id,
             ], 1.0)
-            vol_size.add_metric([vol['id']], vol['size'])
+            vol_size.add_metric([vol.id], vol.size)
 
         yield vol_info
         yield vol_size
